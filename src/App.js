816708d6d73da007c5803b27f32072bfd8a33f33
db001 <dnbennett@hotmail.co.uk>
2018-01-27 23:35:34 +0000

    Basic framework started

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Currency from './Currency';
+
+const URL = 'https://api.coinmarketcap.com/v1/ticker/?convert=GBP&limit=10';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: []
+    };
+  }
+
+  getResults = result => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(result => {
+      this.setState({
+        results: result
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getResults();
+  }
+
   render() {
+
+    const results = this.state.results;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul className="currencies">
+        { results.map(ele =>
+          <Currency
+            key={ele.id}
+            details={ele}
+          />)
+        }
+      </ul>
     );
   }
 }
---
+++ src/Currency.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Currency extends Component {
+
+  render() {
+
+    const details = this.props.details;
+
+    return (
+      <li className="crypto-currency">
+        <h4 className="crypto-title">
+          {details.name}
+        </h4>
+        <p>{details.price_gbp}</p>
+        <div className="percentage-change">
+          <span>{details.percent_change_1h}</span>
+          <span>{details.percent_change_24h}</span>
+          <span>{details.percent_change_7d}</span>
+        </div>
+      </li>
+    )
+
+  }
+}
+
+export default Currency;
